5fb7c9169369e00fa20bad49c254d1488a63d6fa
thajacob <tha.jacob2@gmail.com>
2016-11-10 11:01:34 +0000

    Initial Commit

---
+++ core data single entity swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  core data single entity swift
+//
+//  Created by jakub skrzypczynski on 10/11/2016.
+//  Copyright © 2016 test project. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
